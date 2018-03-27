ce8b436a14f4fdf15618a0477c309f7782d8e82c
Akhil <akhil7687@gmail.com>
2018-03-27 22:12:02 +0530

    news

--- config/routes.rb
+++ config/routes.rb
@@ -22,7 +22,7 @@ Rails.application.routes.draw do
   resources :classified_categories
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root :to =>'home#index'
-  match "/cards"=>"home#feeds", via: [:get]
+  match "/cards"=>"home#feeds", via: [:get],:as=>"cards"
   match "/services"=>"home#services", via: [:get]
   match "/about_us"=>"home#about_us", via: [:get]
   match "/contact_us"=>"home#contact_us", via: [:get]
