13b56e2d0fde9fd6048adf235bd21e8e7a3d2a89
Akhil <akhil7687@gmail.com>
2018-09-11 21:34:26 +0530

    like

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,3 +1,5 @@
 class Like < ApplicationRecord
   belongs_to :user
+  belongs_to :likeable, :counter_cache => true, polymorphic: true
+  validates_uniqueness_of :likeable_id, :scope => [:likeable_type, :user_id]
 end
