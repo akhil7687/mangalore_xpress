53fe6dd5470662a39c3f8a4b9439deebfd913b7b
AKHIL <puru@suphalaam.com>
2018-09-11 17:52:45 +0530

    edit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session, if: Proc.new {|c| c.request.format.json? }
-
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  
   protected
   def after_sign_in_path_for(resource)
   	if current_user.is_admin?
