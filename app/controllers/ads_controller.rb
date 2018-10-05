937f8b69798bf95562ccd089bead05ba29cb7fda
Akhil <akhil7687@gmail.com>
2018-10-05 19:51:59 +0530

    ad api

--- app/controllers/ads_controller.rb
+++ app/controllers/ads_controller.rb
@@ -5,6 +5,11 @@ class AdsController < ApplicationController
   # GET /ads.json
   def index
     @ads = Ad.order("created_at desc")
+    redirect_to do |format|
+      format.json{
+        render :json =>Ad.where("enable=1").as_json(:methods=>[:ad_img])
+      }
+    end
   end
 
   # GET /ads/1
--- app/models/ad.rb
+++ app/models/ad.rb
@@ -2,4 +2,7 @@ class Ad < ApplicationRecord
   has_attached_file :ad_img, styles: { medium: "174x152", thumb: "64x64" }, default_url: lambda { |image| ActionController::Base.helpers.asset_path('hcat3.png')}
   validates_attachment_content_type :ad_img, content_type: /\Aimage\/.*\z/
 
+  def img_url
+    self.ad_img.url(:original)
+  end
 end
