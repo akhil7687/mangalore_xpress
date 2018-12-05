3c1d83d2d60991a252a19ac48595b8c1d9cb65d1
AKHIL <puru@suphalaam.com>
2018-12-05 09:30:43 +0530

    feed

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -157,7 +157,7 @@ class Feed < ApplicationRecord
 
   def self.load_news
     # kannada prabha karnataka
-    urls = ["http://www.kannadaprabha.com/rss/kannada-karnataka-10.xml","http://www.kannadaprabha.com/rss/kannada-nation-4.xml"]
+    urls = ["https://www.kannadaprabha.com/rss/kannada-karnataka-10.xml","https://www.kannadaprabha.com/rss/kannada-nation-4.xml"]
     urls.each do |u|
       Feed.load_from_rss(u,"Kannada Prabha",true,false,false,"Karnataka")
     end
